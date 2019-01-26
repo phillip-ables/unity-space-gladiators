bfd675bfaf30912eb068c6095f93029f21688e2d
lip ables <phillipables1@gmail.com>
2019-01-25 21:07:15 -0500

    get component rigid body on start, public move function that takes a vectory three this just saves it when set by an outside function, on fixed update perform movement function that isnt created yet

--- spaceMultiplayer/Assets/PlayerMotor.cs
+++ spaceMultiplayer/Assets/PlayerMotor.cs
@@ -4,4 +4,20 @@
 public class PlayerMotor : MonoBehaviour {
     private Vector3 velocity = Vector3.zero;
     private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+    }
+    
 }
