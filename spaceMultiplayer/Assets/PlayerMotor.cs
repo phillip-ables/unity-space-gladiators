285614d08c17cb66720b64961e77cd642fc3d927
lip ables <phillipables1@gmail.com>
2019-01-25 21:29:55 -0500

    vector3 zero for rotation declared, public void rotation function to get a vector3 to set our rotation var, on fixed uupdate we call perform rotation a void function that moves rotation from rb rotation times a quaternion euler value of our rotation var

--- spaceMultiplayer/Assets/PlayerMotor.cs
+++ spaceMultiplayer/Assets/PlayerMotor.cs
@@ -3,6 +3,7 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour {
     private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;  // always init to zero
     private Rigidbody rb;
 
     private void Start()
@@ -10,14 +11,22 @@ public class PlayerMotor : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
+    // Gets a movement vector
     public void Move(Vector3 _velocity)
     {
         velocity = _velocity;
+    }
+    
+    // Gets a rotation vector
+    public void Rotation(Vector3 _rotation)
+    {
+        rotation = _rotation;
     }
 
     private void FixedUpdate()
     {
         PerformMovement();
+        PerformRotation();
     }
 
     void PerformMovement()
@@ -26,5 +35,10 @@ public class PlayerMotor : MonoBehaviour {
         {
             rb.MovePosition(rb.position + velocity * Time.deltaTime);
         }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
     }
 }
