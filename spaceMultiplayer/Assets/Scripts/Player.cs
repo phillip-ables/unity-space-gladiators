18d4e32fa2ed937649b1bfd8aa15b9c26fd306e5
lip ables <phillipables1@gmail.com>
2019-01-27 20:21:42 -0500

    private void die sets bool is dead true then will disable components debug log transform name and call respawn method

--- spaceMultiplayer/Assets/Scripts/Player.cs
+++ spaceMultiplayer/Assets/Scripts/Player.cs
@@ -29,10 +29,20 @@ public class Player : NetworkBehaviour {
             return;
         currentHealth -= _amount;
         Debug.Log(transform.name + " now has " + currentHealth + " health.");
-        if(currentHealth <- 0)
+        if (currentHealth <= 0)
         {
             Die();
         }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        //Disable components
+
+        Debug.Log(transform.name + " is DEAD!");
+
+        //call respawn method
     }
 
     public void SetDefaults()
